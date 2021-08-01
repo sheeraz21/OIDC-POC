5a78851ea9fd814400dec9fccc4725978884f427
Sheeraz <er.syedsheerazshaukat@gmail.com>
2021-08-01 16:41:30 +0530

    OIDC first comment

---
+++ oidc-poc/src/main/java/com/oidc/auth/OidcPocApplication.java
@@ -0,0 +1,13 @@
+package com.oidc.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcPocApplication.class, args);
+	}
+
+}
---
+++ oidc-poc/src/test/java/com/oidc/auth/OidcPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oidc.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
